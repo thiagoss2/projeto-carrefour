16be372a84616fab2cf821ad35b0c0017c44cb8d
thiagoss2 <thiagojklb@gmail.com>
2023-10-06 16:32:16 -0300

    atualizando mais coisas

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,15 @@ var cabecalhoSuperior = document.querySelector(".cabecalho-superior");
 
 cabecalhoSuperior.addEventListener('click', function () {
 
-   
+
 });
+
+
+// criando o efeito de modificar o fundo do elemento
+
+var textoLink = document.querySelectorAll(".js-menu-lista-link");
+
+
+
+
+
