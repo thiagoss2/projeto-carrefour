db3a3397a4dc79a63d3aece3f96ed076f6e7fb20
thiagoss2 <thiagojklb@gmail.com>
2023-10-23 14:20:20 -0300

    atualizando estilos

--- elementos.js
+++ elementos.js
@@ -3,8 +3,10 @@ const pagina2 =  fetch("nova-pagina.html");
 
 // pegamos o eelemento da pagina
 
-const div = pagina2.querySelector("texto");
+cons  t div = pagina2.querySelector("texto");
 console.log(div);
+
+console.log(string);
 
 
 
