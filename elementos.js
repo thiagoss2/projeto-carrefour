9d266247a2b8adc37ed68250e93d2668c88dc148
thiagoss2 <thiagojklb@gmail.com>
2023-10-30 16:43:09 -0300

    corrigindo alguns bugs

--- elementos.js
+++ elementos.js
@@ -3,9 +3,11 @@ const pagina2 =  fetch("nova-pagina.html");
 
 // pegamos o eelemento da pagina
 
-cons  t div = pagina2.querySelector("texto");
+const  div = pagina2.querySelector("texto");
 console.log(div);
 
+
+   
 console.log(string);
 
 
